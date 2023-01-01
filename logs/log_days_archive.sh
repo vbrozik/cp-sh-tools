cfcf5b202e5351407e1ff44b24c809c21a250a0f
Václav Brožík <5073939+vbrozik@users.noreply.github.com>
2023-01-01 23:51:45 +0100

    change(log_days_archive): Add time and size log

--- logs/log_days_archive.sh
+++ logs/log_days_archive.sh
@@ -5,6 +5,16 @@
 # For given days show archive size.
 # Use option -d to show the number of days stored. The first and last day can be incomplete.
 
+# The tool shows these columns:
+# date disk_size archive_size time_of_print
+
+# Excel to compute archive size in percent:
+# =($C1 / $B1 / 10)
+
+# Running in the background:
+# nohup log_days_archive.sh &> log_sizes.csv &
+# It seems that using stdbuf -oL is not needed for line buffering.
+
 # The script relies on the default file naming. File names cannot contain spaces.
 
 
@@ -30,6 +40,8 @@ fi
  
 # Print size of logs per day in kB.
 for day in $days ; do
-    printf "%s$SEPARATOR" "$day"
-    tar -cz ./*"$day"_*.log* | wc -c
+    printf "%s$SEPARATOR%s$SEPARATOR%s$SEPARATOR%s\\n" "$day" \
+        "$(du -c ./*"$day"_*.log* | sed -nE 's/^([0-9]+)[ \t]+total.*$/\1/p')" \
+        "$(tar -cz ./*"$day"_*.log* | wc -c)" \
+        "$(date +%T)"
 done
