3ab5e2956ac8191f9c7fcfa1fb707e10114c708d
Václav Brožík <5073939+vbrozik@users.noreply.github.com>
2023-07-07 22:42:29 +0200

    doc(log_daily_storage): Add TODO compute average

--- logs/log_daily_storage.sh
+++ logs/log_daily_storage.sh
@@ -23,13 +23,18 @@ cd "$FWDIR/log/" || errexit "Change to directory failed."
 days=$(
     find . -name '*.log*' |
     sed -nE 's%^\./(.+__)?([0-9]{4}-[0-9]{2}-[0-9]{2})_.*\.log.*%\2%p' |
-    sort -u)
- 
+    sort -u )
+
 # Print the number of days.
 if test "$1" = -d ; then
     echo "$days" | wc -l
 fi
- 
+
+# TODO: Compute the average size of logs per day.
+# Store sizes in an array. Remove the first and last day.
+# Compute the number of whole weeks.
+# Compute the average size of logs per day in all the last whole weeks.
+
 # Print size of logs per day in kB.
 for day in $days ; do
     printf "%s$SEPARATOR" "$day"
