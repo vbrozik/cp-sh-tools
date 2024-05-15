c9c4b83ad6f0455fc13dcd38570a839bb841b05d
Václav Brožík <5073939+vbrozik@users.noreply.github.com>
2024-05-15 16:00:15 +0200

    add(get_vs_interfaces_mtu): Add a tool to check and fix MTU

---
+++ lib_sh/gateways.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Library for obtaining managed gateways
+
+
+# TODO:
+# - get cluster members by cluster
+# - more detailed filtering (e.g. recognize virtual switches)
+# - selection of fields to return (IP address, UUID, etc.)
+
+# Get all gateways of a given type.
+# arguments:
+#   $1: gateway type
+#           simple-gateway          - regular gateway
+#           CpmiVsxClusterNetobj    - VSX cluster
+#           CpmiVsxClusterMember    - VSX cluster member
+#           CpmiVsClusterNetobj     - VS (on a VSX cluster), includes virtual switches!
+# limitations: Does not get more thatn 500 gateways.
+get_gateways () {
+    local gateway_type="$1"
+
+    mgmt_cli -r true show gateways-and-servers limit 500 --format json |
+        jq -r --arg gateway_type "$gateway_type" '
+            .objects[] |
+            select(.type == $gateway_type) |
+            .name'
+}
