cfa8aa089cbaa0e94b73118545b7cebc6fff9d5a
Václav Brožík <5073939+vbrozik@users.noreply.github.com>
2025-03-25 10:34:25 +0100

    improve jq code

--- mgmt_api/examples/tasks.sh
+++ mgmt_api/examples/tasks.sh
@@ -5,6 +5,7 @@
 mgmt_cli -r true show tasks status failed details-level full --format json |
     jq '[
             .tasks[]
+            | select(."task-name" | contains("Policy installation - czobl_policy"))
             | {
                 "task-name",
                 "creation-time-iso": ."meta-info"."creation-time"."iso-8601",
@@ -20,9 +21,20 @@ mgmt_cli -r true show tasks status failed details-level full --format json |
                         | {
                             "creation-time-iso": $task_details["meta-info"]["creation-time"]["iso-8601"],
                             "last-modify-time-iso": $task_details["meta-info"]["last-modify-time"]["iso-8601"],
-                            "stage": $stage.stage,
-                            "message": .message
+                            gatewayId: $task_details.gatewayId,
+                            stage: $stage.stage,
+                            message
                             }
                     ]
             } | select(.["error-messages"] | length > 0)
         ] | sort_by(.["creation-time-iso"])'
+
+mgmt_cli -r true show tasks status failed details-level full --format json |
+    jq '[
+            .tasks[]
+            | select(."task-name" | contains("Policy installation - czobl_policy"))
+        ] | sort_by(.["creation-time-iso"])'
+
+# TODO: Dictionary of gateways UID -> name
+# fails:
+# show objects type CpmiVsClusterMember
